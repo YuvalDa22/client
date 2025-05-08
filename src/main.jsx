efc7b6998450e960b846f1ad7145069f8c78c080
Yuval Dadon <yuvidadon@gmail.com>
2025-05-08 14:08:14 +0300

    env & package fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-
-
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
