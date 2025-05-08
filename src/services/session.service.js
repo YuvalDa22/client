c95483a60575f4c99d9c4225ff6be94c9c9853f3
Yuval Dadon <yuvidadon@gmail.com>
2025-05-08 13:03:35 +0300

    update port to match server

--- src/services/session.service.js
+++ src/services/session.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'https://localhost:5000/api/session';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://localhost:5001/api/session';
 
 export async function createSession(userData){
     return await axios.post(`${BASE_URL}/create`, userData)
