ff7383749fa7a83e868027e5786ff57fc29eb285
Yuval Dadon <yuvidadon@gmail.com>
2025-05-09 14:18:00 +0300

    match path to server

--- src/services/session.service.js
+++ src/services/session.service.js
@@ -4,4 +4,5 @@ const API = import.meta.env.VITE_API_URL || 'http://localhost:5001/api';
 
 export const createSession = (data) => axios.post(`${API}/session/create`, data);
 
-export const joinSession = (sessionId, data) => axios.put(`${API}/session/${sessionId}/join`, data);
+export const joinSession = (sessionId, data) => axios.put(`${API}/session/join/${sessionId}`, data);
+
